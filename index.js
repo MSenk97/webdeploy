a4e89dd75edab9e08af4da0b12a70bb141d3970a
MSenk97 <lolarmmagedonn@gmail.com>
2021-09-20 18:09:33 +0200

    message

--- index.js
+++ index.js
@@ -33,14 +33,14 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
+app.post("/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/categories", categoryRoute);
+app.use("/auth", authRoute);
+app.use("/users", userRoute);
+app.use("/posts", postRoute);
+app.use("/categories", categoryRoute);
 
 app.listen(process.env.PORT, () => {
   console.log("Backend is running.");
